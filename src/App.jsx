2ac1c15332125d3cdac98166aef8ef8285e1d39b
yashsoni45 <yashraj4549@gmail.com>
2024-08-11 03:11:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Cart from "./pages/Cart";
 
 const App = () => {
   return (
-    <div>
+    <div className="overflow-x-hidden">
     <div className="bg-slate-900">
          <Navbar />
     </div>
